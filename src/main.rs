640b8456d90352d1658e84115e2caae05f100185
Daniel P. Clark <6ftdan@gmail.com>
2017-09-20 22:10:33 -0400

    Input parsing complete
    Example: cargo run --bin abrute 4 asdf target
    
    Next is to make it execute decryption attempts in sequence.  Then to
    implement threading attempts with multiple cores.  Then to implement a
    RAM drive in which to do the attempts in.  Then add log file
    integration.  And the last feature to implement is a pooling server and
    clients for multiple computers to work on the same task.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,69 @@
 #[allow(unused_imports)]
 extern crate digits;
+extern crate array_tool;
 extern crate rayon;
 // extern crate array_fire;
 extern crate num_cpus;
-#[macro_use]
-extern crate clap;
+use digits::{BaseCustom, Digits};
+use array_tool::vec::{Times,Join};
+use std::env;
+use std::ffi::OsString;
+use std::io::{self, Write}; 
 
-use clap::App;
 use rayon::prelude::*;
 
-fn main() {
+fn derive_min_max(range: OsString) -> (i32, i32) {
+  let rvals = range.to_str().unwrap().split(':').collect::<Vec<&str>>();
+  let mut rivals = rvals.iter();
+  let min = rivals.next().unwrap();
+  let max = rivals.next();
+  let min = min.parse::<i32>().unwrap();
+  let get_max = || -> i32 {
+    match max {
+      Some(v) => { v.parse::<i32>().unwrap() },
+      _ => { min }
+    }
+  };
+  (min, get_max())
+}
+
+fn derive_character_base(characters: OsString) -> BaseCustom<char> {
+  let chrs = characters.to_str().unwrap();
+  BaseCustom::<char>::new(chrs.chars().collect())
+}
+
+fn fill_with_zero<'a>(d: Digits<'a>, c: i32) -> Digits<'a> {
+  d.propagate(vec![d.zero().to_s()].times(c).join(""))
+}
+
+fn run_app() -> Result<(), &'static str> {
   let cpus = num_cpus::get();
-  let yaml = load_yaml!("../cli.yml");
-  let matches = App::from_yaml(yaml).get_matches();
+  let mut args = env::args_os();
+  args.next();
+
+  if args.len() < 3 {
+    return Err(("Invalid arguments provided."));
+  }
+
+  let (min, max) = derive_min_max(args.next().unwrap());
+  let mapping = derive_character_base(args.next().unwrap());
+  let mut sequencer = Digits::new(&mapping, "".to_string());
+  sequencer = fill_with_zero(sequencer, min);
+  let target = args.next_back().unwrap();
+
+  println!("min: {:?}\nmax: {:?}\ncharacters: {:?}\ntarget: {:?}", min, max, sequencer.to_s(), target);
+
+  Ok(())
+}
+
+fn main() {
+  ::std::process::exit(
+    match run_app() {
+      Ok(_) => 0,
+      Err(err) => {
+        writeln!(io::stderr(), "error: {:?}", err).unwrap();
+        1
+      }
+    }
+  );
 }
