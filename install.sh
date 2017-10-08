95fc8ef4ed6b517ed69ebbca3004af7513d49f8c
Baptiste <baptiste.colin@outlook.com>
2017-10-08 15:20:38 +0200

    adds proper aescrypt installation

--- install.sh
+++ install.sh
@@ -11,24 +11,40 @@ then
 else
     # Rust is not installed
     # Ask for user consent before proceeding with installation
-    read -p "Rust is not installed. Do you want to install Rust now ? (necessary for Abrute) [y/n] " answer
+    read -p "Rust is not installed. Do you want to install Rust now ? (necessary for Abrute) [y/n] " Rust_answer
 
-    if [ $answer == y ];
+    if [ $Rust_answer == y ];
     then
         # User agreed, proceed with installation
-        curl https://sh.rustup.rs -sSf | sh
+        # curl https://sh.rustup.rs -sSf | sh
+        echo "là on installe Rust"
     else
         # User refused, abort installation
         exit 1
     fi
 fi
 
+# Checking if Aescrypt is installed
+if [ -f /usr/bin/aescrypt ] && [ -f /usr/bin/aescrypt_keygen ];
+then
+    # Aescrypt is already installed
+    echo "Aescrypt already installed"
+else
+    read -p "Aescrypt is not installed. Do you want to install Aescrypt now ? (necessary for Abrute) [y/n] " AES_answer
 
-# installing aescrypt
-wget https://www.aescrypt.com/download/v3/linux/aescrypt-3.13.tgz
-tar -xzf aescrypt-3.13.tgz
-cd aescrypt-3.13/src
-make && sudo make install
-cd ../.. && rm -rf aescrypt-3.13
+    if [ $AES_answer == y ];
+    then
+        User agreed, proceed with installation
+        wget https://www.aescrypt.com/download/v3/linux/aescrypt-3.13.tgz
+        tar -xzf aescrypt-3.13.tgz
+        cd aescrypt-3.13/src
+        make && sudo make install
+        cd ../.. && rm -rf aescrypt-3.13 && rm aescrypt-3.13.tgz
+    else
+        # User refused, abort installation
+        exit 1
+    fi
+fi
+
 
 
