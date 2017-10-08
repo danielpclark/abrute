be61920a267906d62182a92c8dbb4146b0057501
Baptiste <baptiste.colin@outlook.com>
2017-10-08 12:52:35 +0200

    checks if Rust is installed before doing anything, and asks user if he agrees to install Rust before doing so

--- install.sh
+++ install.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+rust_install_status=`rustc --version`
+rustc_prefix="rustc"
+
+# Checking if Rust is installed
+if [[ $rust_install_status == $rustc_prefix* ]];
+then
+    # Rust is installed
+    echo "Rust already installed"
+else
+    # Rust is not installed
+    # Ask for user consent before proceeding with installation
+    read -p "Rust is not installed. Do you want to install Rust now ? (necessary for Abrute) [y/n] " answer
+
+    if [ $answer == y ];
+    then
+        # User agreed, proceed with installation
+        curl https://sh.rustup.rs -sSf | sh
+    else
+        # User refused, abort installation
+        exit 1
+    fi
+fi
+
+
 # installing aescrypt
 wget https://www.aescrypt.com/download/v3/linux/aescrypt-3.13.tgz
 tar -xzf aescrypt-3.13.tgz
@@ -7,6 +31,4 @@ cd aescrypt-3.13/src
 make && sudo make install
 cd ../.. && rm -rf aescrypt-3.13
 
-# installing Rust
-curl https://sh.rustup.rs -sSf | sh
 
